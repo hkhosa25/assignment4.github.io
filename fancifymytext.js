81542ef072d23cbadcf9a0ea8ab496461300e9c2
hkhosa25 <hkhosa@csus.edu>
2021-12-10 02:31:01 -0800

    files
    files

---
+++ fancifymytext.js
@@ -0,0 +1,22 @@
+function myButton() {
+	alert("Hello, world!");
+	document.getElementById("Text").style.fontSize = "24px";
+}
+
+function myRadio() {
+	document.getElementById("Text").style.fontWeight = "900";
+	document.getElementById("Text").style.color = "blue";
+	document.getElementById("Text").style.textDecoration = "underline";
+}
+
+function myRadio2() {
+	document.getElementById("Text").style.fontWeight = "400";
+}
+
+function myMoo() {
+	document.getElementById("Texr").style.textTransform = "capitalize";
+
+}
+
+
+
